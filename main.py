a9ccf2b1cdf401ed59242b1c690740784397f2c8
thatbrguy <bharathrajn98@gmail.com>
2018-12-09 00:56:11 +0530

    refactor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import argparse
 import numpy as np
 from collections import Counter
 from utils import load_data, mat2array
@@ -13,10 +14,13 @@ from keras.callbacks import ModelCheckpoint, Callback
 from keras.layers import Conv2DTranspose, Add, Input, Concatenate, Layer, SeparableConv2D
 from keras.layers import Dense, Activation, BatchNormalization, Dropout, LeakyReLU, Conv2D, Reshape
 
-import matplotlib.pyplot as plt
-
-# Indian_pines_corrected.mat
-# Indian_pines_gt.mat
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, help='Number of epochs', default=100)
+parser.add_argument('--batch_size', type=int, help='Batch size', default=200)
+parser.add_argument('--lr', type=float, help='Learning Rate', default=0.001)
+parser.add_argument('--model', help='Choose between models "A" and "B"', choices=['A', 'B'], required=True)
+parser.add_argument('--mode', help='Choose between modes "train" and "infer"', choices=['train', 'infer'], required=True)
+parser.add_argument('--weights', help='Path of the weights file for inference mode', default='')
 
 class statsLogger(Callback):
     """
@@ -129,7 +133,7 @@ def separable_model(img):
 
     return x
 
-def build_model(mode, input_shape):
+def build_model(model, input_shape):
     """
     Builds the model based on your choice of the mode
     variable.
@@ -138,44 +142,54 @@ def build_model(mode, input_shape):
 
     img = Input(shape = input_shape)
 
-    if mode == 'conv':
-        x = conv_model(img)
-    elif mode == 'separable':
+    if model == 'A':
         x = separable_model(img)
+    elif model == 'B':
+        x = conv_model(img)
 
     x = PixelSoftmax(axis=-1)(x)
     model = Model(inputs=img, outputs=x)
 
     return model
 
-def train(mode):
+def train(args):
 
     trainX, valX, trainY, valY, weights_dict = load_data()
 
-    model = build_model(mode, input_shape = trainX.shape[1:])
+    model = build_model(args.model, input_shape = trainX.shape[1:])
+
+    """
+    # Converting shape of trainY, valY from [-1, 4, 4, 16] to [-1, 16, 16]
+    # That is, reshaping [None, H, W, C] to [None, H * W, C]
+    # This is done to weight the loss function classwise using "sample_weights"
+    # Refer: https://github.com/keras-team/keras/issues/3653
+    """
 
     sample_weights = []
-
-    # Converting shape of trainY, valY from [-1, 4, 4, 16] to [-1, 16, 16]
-    # This is done to provide classwise weighted regularization
-
     valY = np.reshape(valY, (-1, 16, 16))
     trainY = np.reshape(trainY, (-1, 16, 16))
 
-    # Applying sample weights
-    # TODO: More explanation
+    """
+    # Applying sample_weights as class_weights are not supported for 3+ dimensional
+    # targets. We provide a weight value from the weights_dict to each pixel of the
+    # 4x4 patch. As explained above, the 4x4 patch is reshaped to a 16-D vector. We
+    # pass a 16-D weight_vector to provide pixel-wise loss function weightage, for
+    # each training sample. The set of 16-D weight vectors for all training samples
+    # is contained in sample_weights.  
+    """
+
     for i in range(len(trainY)):
-      array = trainY[i]
-      temp_weight = []
-      for j in range(len(array)):
-        temp_weight.append(weights_dict[j])
-      sample_weights.append(temp_weight)
+      patch = trainY[i]
+      weight_vector = []
+      for j in range(len(patch)):
+        weight_vector.append(weights_dict[patch[j].argmax()])
+      sample_weights.append(weight_vector)
 
     sample_weights = np.array(sample_weights)
 
     history = statsLogger()
-    opt = Adam(lr = 0.001, decay = 1e-4)
-    ckpt = ModelCheckpoint(filepath = mode + '.hdf5', 
+    opt = Adam(lr = args.lr, decay = 1e-4)
+    ckpt = ModelCheckpoint(filepath = args.model + '.hdf5', 
                            save_best_only = True, 
                            verbose = 1, 
                            monitor = 'val_loss')
@@ -187,17 +201,17 @@ def train(mode):
     
     model.fit(trainX, 
               trainY, 
-              batch_size = 200, 
-              epochs = 100, 
+              batch_size = args.batch_size, 
+              epochs = args.epochs, 
               validation_data = (valX, valY), 
               callbacks = [ckpt, history], 
               sample_weight = sample_weights)
 
     # Dumping logs into a pickle file
-    with open(mode + '.pickle', 'wb') as file:
+    with open(args.model + '_logs.pickle', 'wb') as file:
         pickle.dump(history.logs, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-def inference(mode, weights):
+def inference(args):
 
     data, gt = mat2array()
 
@@ -205,8 +219,8 @@ def inference(mode, weights):
     output = np.zeros((145, 145, 16))
 
     clear_session()
-    model = build_model(mode, input_shape = (4, 4, 200))
-    model.load_weights(weights)
+    model = build_model(args.model, input_shape = (4, 4, 200))
+    model.load_weights(args.weights)
 
     for i in range(0, data.shape[0] - patch_size, patch_size):
         for j in range(0, data.shape[1] - patch_size, patch_size):
@@ -222,8 +236,6 @@ def inference(mode, weights):
 
     # Output after filtering 
     masked_output = (output_semantic * gt_mask)
-
-    plt.imsave('output.png', masked_output)
 
     count = 0
     total = 0
@@ -257,10 +269,16 @@ def inference(mode, weights):
     formatted_string = '\nOverall Accuracy = %.3f \nAverage Accuracy = %.3f \nKappa Score = %.3f \n'
     print(formatted_string % (overall_acc * 100, average_acc * 100, kappa_score * 100))
 
-    print(classwise_prediction_frequency)
 
 if __name__ == '__main__':
 
-    #train('separable')
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    inference('conv', 'balanced_conv.hdf5')
+    args = parser.parse_args()
+    
+    if args.mode == 'train':
+        train(args)
+    if args.mode == 'infer':
+        if os.path.exists(args.weights):
+            inference(args)
+        else:
+            print('Please specify a valid path for the weights file if you want to use ', end="")
+            print('inference mode. You can do this by setting the --weights argument')
